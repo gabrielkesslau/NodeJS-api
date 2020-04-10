afa8e3ef29c087f31e028275d0e6d57df7c14d69
Gabriel Kesslau <gabrielkesslau@gmail.com>
2020-04-10 02:04:39 -0300

    node-api

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const requireDir = require("require-dir"); //npm install require-dir
+
+// iniciando o app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+// iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi', 
+    { useNewUrlParser: true, useUnifiedTopology: true },
+);
+
+// require('./src/models/Product');
+requireDir("./src/models");
+
+// const Product = mongoose.model('Product');
+
+// Rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
